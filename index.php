c2684d8eb96607d790df61e8cf63501f710933e5
Gabriel Viana <gaa.vianaa@gmail.com>
2025-04-08 11:21:56 -0300

    chamando a classe

--- index.php
+++ index.php
@@ -8,6 +8,18 @@
 <body>
     <h1>PHP com POO - Exemplo 01</h1>
     <hr>
-    
+
+
+<?php
+// Carregando/importando a classe
+require_once "src/Cliente.php";
+
+$clienteA = new Cliente();
+$clienteB = new Cliente();
+
+?>
+
+    <h2>Testes de acesso aos recursos da classe através do objeto</h2>
+    <pre><?=var_dump($clienteA, $clienteB)?></pre>
 </body>
 </html>
--- src/Cliente.php
+++ src/Cliente.php
@@ -1,10 +1,17 @@
 <?php
 
-class Animal
+class Cliente
 {
     public string $nome;
     public int $idade;
     public string $email;
+
+    public function definirDados($nome, $idade, $email)
+    {
+        $this->nome = $nome;  
+        $this->idade = $idade;  
+        $this->email = $email;  
+    }
 
     public function exibirDados(): void
     {
