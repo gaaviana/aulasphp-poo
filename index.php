49240a7a3ea847c8600e2e1e06d2ab17fcee58fd
Gabriel Viana <gaa.vianaa@gmail.com>
2025-04-08 11:42:35 -0300

    inicializa e acessa clienteB

--- index.php
+++ index.php
@@ -21,6 +21,10 @@ $clienteA->nome = "Gabriel";
 $clienteA->idade = 18;
 $clienteA->email = "Gabriel@gmail.com";
 
+$clienteB->nome = "Viana";
+$clienteB->idade = 22;
+$clienteB->email = "viana@gmail.com";
+
 ?>
     <h2>Acessando/lendo os dados dos objetoss</h2>
     <h3>Cliente A</h3>
@@ -32,6 +36,17 @@ $clienteA->email = "Gabriel@gmail.com";
     <ul>
         <li><b>Idade: </b><?=$clienteA->idade?></li>
         <li><b>Email: </b><?=$clienteA->email?></li>
+    </ul>
+
+    <h3>Cliente B</h3>
+    <div>
+        <?php
+        $clienteB->exibirDados();
+        ?>
+    </div>
+    <ul>
+        <li><b>Idade: </b><?=$clienteB->idade?></li>
+        <li><b>Email: </b><?=$clienteB->email?></li>
     </ul>
 
     <h2>Testes de acesso aos recursos da classe através do objeto</h2>
