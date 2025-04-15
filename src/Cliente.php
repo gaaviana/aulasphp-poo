95d1924215db37ac5b099c7618ddbe3b0398d5b9
Gabriel Viana <gaa.vianaa@gmail.com>
2025-04-15 09:39:32 -0300

    refatora if

--- src/Cliente.php
+++ src/Cliente.php
@@ -17,13 +17,10 @@ class Cliente
 
     public function verificarIdade(): string
     {
-        if ($this->idade< 18) {
-            return "Menor de idade";
-        } elseif ($this->idade < 60) {
-            return "Adulto";
-        } else {
-            return "Idoso";
-        }
+        if ($this->idade< 18) return "Menor de idade";
+        if ($this->idade < 60) return "Adulto";
+        return "Idoso";
+    
     }
 
     public function setNome(string $nome):void 
