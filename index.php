e02e28d9a20a3c7cdf35fef1748b2e3e1ffcf804
Gabriel Viana <gaa.vianaa@gmail.com>
2025-04-17 11:00:01 -0300

    cliente PJ

--- index.php
+++ index.php
@@ -14,12 +14,16 @@
 require_once "src/Cliente.php"; // superClasse
 require_once "src/PessoaFisica.php"; // SubClasse
 require_once "src/Situacao.php"; // Enum
+require_once "src/PessoaJuridica.php"; // subClasse
 
 $clientePF = new PessoaFisica("gab", "gab@gmail.com", 19, "571.931.358-30
  ");
+
+ $clientePJ = new PessoaJuridica("viana", "viana@gmail.com", "12.345.678/0001-00", 2010, "V.Airways")
 ?>
 
 <pre><?=var_dump($clientePF)?></pre>
+<pre><?=var_dump($clientePJ)?></pre>
     
 </body>
 </html>
